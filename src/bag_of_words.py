14c71986d7d8c05e5e7edbb69b781f0dacab0b35
t1pUr <62817154+t1pUr@users.noreply.github.com>
2021-05-15 22:56:57 +0300

    Create bag_of_words.py

---
+++ src/bag_of_words.py
@@ -0,0 +1,44 @@
+import nltk
+import bs4 as bs
+import urllib.request
+import re
+
+raw_html = urllib.request.urlopen('https://en.wikipedia.org/wiki/Python_(programming_language)')
+raw_html = raw_html.read()
+article_html = bs.BeautifulSoup(raw_html, 'lxml')
+article_paragraphs = article_html.find_all('p')
+article_text = ''
+
+for para in article_paragraphs:
+    article_text += para.text
+
+corpus = nltk.sent_tokenize(article_text)
+
+for i in range(len(corpus )):
+    corpus [i] = corpus [i].lower()
+    corpus [i] = re.sub(r'\W',' ',corpus [i])
+    corpus [i] = re.sub(r'\s+',' ',corpus [i])
+
+print(corpus[2])
+print(len(corpus))
+
+bag_of_words = {}
+for sentence in corpus:
+    tokens = nltk.word_tokenize(sentence)
+    for token in tokens:
+        if token not in bag_of_words.keys():
+            bag_of_words[token] = 1
+        else:
+            bag_of_words[token] += 1
+print(bag_of_words)
+
+pairs = []
+list_of_words = bag_of_words.keys()
+for i in list_of_words:
+    pairs.append([i, bag_of_words[i]])
+
+sorted_bag_of_words = reversed(sorted(pairs, key=lambda sort: sort[1]))
+for i in sorted_bag_of_words:
+    print(f"{i[0]} - {i[1]}")
+
+
